20426c793f8f285f8a16373ddbf3dea61228a462
JimmyGre <rygspoker@gmail.com>
2020-10-01 16:57:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue("Jimmy"));
+console.log(chalk.blue("Laura"));
+console.log(chalk.green("Thomax"));
+console.log(chalk.blue("Florian"));
+console.log(chalk.blue("remy"));
